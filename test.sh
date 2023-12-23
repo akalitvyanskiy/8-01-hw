87236eab80c3c4dac988a700ab041c3d3b2639b2
akalitvyanskiy <wateruser161@yandex.ru>
2023-12-23 11:57:09 +0300

    commit 7 dev

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-1. Запись 1 commit 1
-2. Запись 2 commit 2
-3. Запись 3 commit 3
-4. Запись 4 commit 4
-5. Запись 5 commit 5
-6. Запись 6 coomit 6
+1. Запись 1 commit 1 dev
+2. Запись 2 commit 2 dev
+3. Запись 3 commit 3 dev
+4. Запись 4 commit 4 dev
+5. Запись 5 commit 5 dev
+6. Запись 6 commit 6 dev
+7. Запись 7 commit 7 dev
